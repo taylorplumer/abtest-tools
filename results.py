7128423ad3e8d0661e7ef980ea588e7f95d34786
taylorplumer <taylorplumer@gmail.com>
2020-05-06 21:52:52 -0700

    Update to bootsrap and fix type errors

--- results.py
+++ results.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import dash
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input,Output, State
 import pandas as pd
@@ -9,24 +10,27 @@ from min_sample_size import min_sample_size
 from create_row import create_row
 from scipy import stats
 from helpers import create_curve, create_plot, calculate_pvalue
+from app import create_form
+from scipy import stats, special
 
 
 test_data_keys = ['Visitors A', 'Conversions A', 'Visitors B', 'Conversions B']
-default_values = [100000, 3000, 100000, 3200]
+default_values = [100000, 3000, 100000, 3800]
 default_dict = dict(zip(test_data_keys, default_values))
-app = dash.Dash()
 
-header_ =     html.Div(
-                 className="app-header",
-                 children=[
-                     html.Div('AB Test Results Significance Calculator', className="app-header--title")
-                 ]
-             )
+app = dash.Dash(external_stylesheets=[dbc.themes.MATERIA])
 
 
-test_type = html.Div([
+navbar = dbc.NavbarSimple(
+    brand="AB Test Results Significance Calculator",
+    brand_href="#",
+    color="primary",
+    dark=True
+)
+form = create_form(default_dict)
+test_type = dbc.Container(html.Div([
                 html.P('Test Type'),
-                dcc.RadioItems(id= 'test-type-radio',
+                dbc.RadioItems(id= 'test-type-radio',
                     options=[
                         {'label': 'One-sided', 'value': 'One-sided'},
                         {'label': 'Two-sided', 'value': 'Two-sided'}
@@ -34,11 +38,11 @@ test_type = html.Div([
                     value = 'One-sided',
                     labelStyle={'display': 'inline-blickk'}
                     )
-])
+]))
 
-confidence_level = html.Div([
+confidence_level = dbc.Container(html.Div([
                 html.P('Confidence Level'),
-                dcc.RadioItems(id= 'confidence-level-radio',
+                dbc.RadioItems(id= 'confidence-level-radio',
                     options = [
                         {'label': '90%', 'value': 0.90},
                         {'label': '95%', 'value': 0.95},
@@ -47,41 +51,29 @@ confidence_level = html.Div([
                     value = 0.95,
                     labelStyle={'display': 'inline-blickk'}
                     )
-])
+]))
 
+output_text = dbc.Container(html.H2(id='results-text', children="The Results are..."))
+output_graph = dbc.Container(dcc.Graph(id = 'results-graph'))
 
-app.layout= html.Div([
-            header_,
-             dcc.Markdown('''
-             **Are your test results significant?** Input your test data below.
-             '''),
-             html.Div([
-                 create_row(key,value) for key, value in default_dict.items()
-                 ]),
-            html.Button('Submit', id='submit-button'),
-            html.Div([
-                test_type,
-                confidence_level
-            ]),
-            html.Div([
-                html.H2(id='results-text', children="The Results are...")
-            ]),
-            html.Div([
-                dcc.Graph(id = 'results-graph')
-            ])
-             ])
-
+app.layout = html.Div([navbar,
+                        form,
+                        test_type,
+                        confidence_level,
+                        output_text,
+                        output_graph
+                    ])
 
 @app.callback([Output('results-graph', 'figure'), Output('results-text', 'children')],
                 [Input('submit-button', 'n_clicks'), Input('confidence-level-radio', 'value')],
                 [State(element, 'value') for element in test_data_keys])
 
 def update_results(n_clicks, confidence_, control_visitors, control_conversions, treatment_visitors, treatment_conversions):
-    control_mu = int(control_conversions)/int(control_visitors)
-    treatment_mu = int(treatment_conversions)/int(treatment_visitors)
-    difference_mu = abs(int(control_mu - treatment_mu))
-    control_x, control_y, control_sd = create_curve(mu=int(control_conversions)/int(control_visitors), visitors=int(control_visitors))
-    treatment_x, treatment_y, treatment_sd = create_curve(mu=int(treatment_conversions)/int(treatment_visitors), visitors=int(treatment_visitors))
+    control_mu = float(control_conversions)/float(control_visitors)
+    treatment_mu = float(treatment_conversions)/float(treatment_visitors)
+    difference_mu = abs(float(control_mu - treatment_mu))
+    control_x, control_y, control_sd = create_curve(mu=float(control_conversions)/float(control_visitors), visitors=int(control_visitors))
+    treatment_x, treatment_y, treatment_sd = create_curve(mu=float(treatment_conversions)/float(treatment_visitors), visitors=int(treatment_visitors))
 
     figure = create_plot(control_x, control_y, treatment_x, treatment_y, control_mu, control_sd)
 
@@ -97,13 +89,13 @@ def update_results(n_clicks, confidence_, control_visitors, control_conversions,
             text_1 = "Variation A's observed conversion rate ({}) was {} higher than Variation B's conversion rate ({})."\
             .format(str(round(control_mu*100, 2))+'%',str(round(relative_uplift*100, 2)) + '%', str(round(treatment_mu*100, 2))+'%')
 
-
-        p_value = calculate_pvalue(difference_mu, control_sd, treatment_sd)
+        z_score = difference_mu/np.sqrt(treatment_sd**2 + control_sd**2)
+        p_value = 1- special.ndtr(z_score)
 
         if p_value < (1-confidence_):
             text_2 = "You can be {} confident that {} has a higher conversion rate.".format(str((float(confidence_) * 100))+'%', winning_variation)
         else:
-            text_2 = " The difference however isn't sufficicient, so you cannot be confident that this result is a consequence of the treatment."
+            text_2 = " The difference however isn't sufficient, so you cannot be confident that this result is a consequence of the treatment."
 
         return text_1, text_2
 
